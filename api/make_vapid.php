31c09388311b8f45225c018f7bebedc56f32cf54
gsandu18 <gsandu614@gmail.com>
2025-09-06 21:22:50 +0200

    Delete api/make_vapid.php

--- api/make_vapid.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-use Minishlink\WebPush\VAPID;
-$keys = VAPID::createVapidKeys();
-echo "PUBLIC=".$keys['publicKey']."\nPRIVATE=".$keys['privateKey']."\n";
