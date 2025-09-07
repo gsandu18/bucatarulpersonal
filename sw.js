9a953bae36d28f830f9680ef1e5b983e32cec3ec
gsandu18 <gsandu614@gmail.com>
2025-09-07 11:33:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,40 @@
-const PATH_PREFIX = "/bucatarulpersonal";
+// /sw.js
 const CACHE = "bp-v1";
 const ASSETS = [
-  `${PATH_PREFIX}/`,
-  `${PATH_PREFIX}/index.html`,
-  `${PATH_PREFIX}/manifest.webmanifest`
+  "/bucatarulpersonal/",
+  "/bucatarulpersonal/index.html",
+  "/bucatarulpersonal/manifest.webmanifest",
+  "/bucatarulpersonal/img/icon-192.png",
+  "/bucatarulpersonal/img/icon-512.png"
 ];
 
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
-self.addEventListener('fetch', e=>{
-  if (e.request.method !== 'GET') return;
+
+self.addEventListener("fetch", e => {
+  const { request } = e;
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res=>{
-      const copy = res.clone();
-      caches.open(CACHE).then(cache=>cache.put(e.request, copy)).catch(()=>{});
-      return res;
-    }).catch(()=> caches.match(`${PATH_PREFIX}/index.html`)))
+    caches.match(request).then(res =>
+      res ||
+      fetch(request).then(net => {
+        // cache-on-the-fly (doar GET)
+        if (request.method === "GET") {
+          const copy = net.clone();
+          caches.open(CACHE).then(c => c.put(request, copy));
+        }
+        return net;
+      }).catch(() => caches.match("/bucatarulpersonal/index.html"))
+    )
   );
 });
